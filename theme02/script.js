124637b00a86e4fd75818a7360706788155d2919
Daniil Gannota <daniil.gannota.contact@gmail.com>
2016-08-18 13:40:46 +0300

    0.5alpha2

---
+++ theme02/script.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){	
+	$('.transition').click(function() {
+		$('.featured').toggle();
+	});
+	
+	
+	$('.transition_back').click(function() {
+		$('.featured_return').toggle();
+	});
+});
