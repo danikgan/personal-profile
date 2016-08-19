5f91d13ae32ea4ecebb5629389cd887fcf771fc0
Daniil Gannota <daniil.gannota.contact@gmail.com>
2016-08-19 23:48:20 +0300

    - code optimisations

--- theme02/script.js
+++ theme02/script.js
@@ -1,71 +1,21 @@
 $('document').ready(function(){
+	
 	/*----------------------------------- Loading -----------------------------------*/
-    $(document).ajaxStart(function(){
-        $("#wait").css("display", "block");
-        $( ".inside" ).html("Loading...");
-    });
-    $(document).ajaxComplete(function(){
-        $("#wait").css("display", "none");
-    });
+    $(document).ajaxStart(function(){$("#wait").css("display", "block");$( ".inside" ).html("Loading...");});
+    $(document).ajaxComplete(function(){$("#wait").css("display", "none");});
 	
 	
-	/*----------------------------------- Content Loading -----------------------------------*/
-	$('.menEdu').click(function() {
-		$.ajaxSetup ({cache: false});
-		$( ".inside" ).load( "../content/content.html #education");
-			
-		$('.featured').toggle();
-	});
-	
-	
-	$('.menTechSkills').click(function() {
-		$.ajaxSetup ({cache: false});
-		$( ".inside" ).load( "../content/content.html #techskills");
-			
-		$('.featured').toggle();
-	});	
-	
-	
-	$('.menWorkExp').click(function() {
-		$.ajaxSetup ({cache: false});
-		$( ".inside" ).load( "../content/content.html #workexp");
-			
-		$('.featured').toggle();
-	});
-	
-	
-	$('.menProj').click(function() {
-		$.ajaxSetup ({cache: false});
-		$( ".inside" ).load( "../content/content.html #proj");
-			
-		$('.featured').toggle();
-	});
-	
-	
-	$('.menCovLet').click(function() {
-		$.ajaxSetup ({cache: false});
-		$( ".inside" ).load( "../content/content.html #covlet");
-			
-		$('.featured').toggle();
-	});
-	
-	
-	$('.menPerStat').click(function() {
-		$.ajaxSetup ({cache: false});
-		$( ".inside" ).load( "../content/content.html #perstat");
-			
-		$('.featured').toggle();
-	});
+	/*----------------------------------- Content (loaded externally) -----------------------------------*/
+	$.ajaxSetup ({cache: false});
+	$('.menEdu').click(function() {$( ".inside" ).load( "../content/content.html #education");	$('.featured').toggle();});
+	$('.menTechSkills').click(function() {$( ".inside" ).load( "../content/content.html #techskills");	$('.featured').toggle();});	
+	$('.menWorkExp').click(function() {$( ".inside" ).load( "../content/content.html #workexp");	$('.featured').toggle();});
+	$('.menProj').click(function() {$( ".inside" ).load( "../content/content.html #proj");	$('.featured').toggle();});
+	$('.menCovLet').click(function() {$( ".inside" ).load( "../content/content.html #covlet");	$('.featured').toggle();});
+	$('.menPerStat').click(function() {$( ".inside" ).load( "../content/content.html #perstat");	$('.featured').toggle();});
 	
 	
 	/*----------------------------------- Simple Transition: when to be closed -----------------------------------*/
-	$('.transition').click(function() {
-		$('.featured').toggle();
-	});
-	
-	$('.transition_back').click(function() {
-		$('.featured_return').toggle();
-	});
-	
-	
+	$('.transition').click(function() {$('.featured').toggle();});
+	$('.transition_back').click(function() {$('.featured_return').toggle();});
 });		
