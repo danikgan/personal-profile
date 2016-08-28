0604b63247d09a49fc656ef004b1ffd1c709eccd
Daniil Gannota <daniil.gannota.contact@gmail.com>
2016-08-29 00:16:41 +0300

    5.2alpha
     - testing Education

--- content/2.js
+++ content/2.js
@@ -1,5 +1,5 @@
 $('document').ready(function(){
-	
+    
     /*----------------------------------- More Information In Education Section -----------------------------------*/    
 	$('.moreEdu_click').click(function() {$(".moreEducation").toggle();});
 });		
