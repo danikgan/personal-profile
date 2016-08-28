26d04528954b7080d57106bd5861524a8cde4c5f
Daniil Gannota <daniil.gannota.contact@gmail.com>
2016-08-29 00:57:07 +0300

    0.5.2alpha2
     - added optimisations for the starting page

--- content/2.js
+++
@@ -1,5 +0,0 @@
-$('document').ready(function(){
-    
-    /*----------------------------------- More Information In Education Section -----------------------------------*/    
-	$('.moreEdu_click').click(function() {$(".moreEducation").toggle();});
-});		
