1e8871269656eb329d933212c8baedff3396bcff
Daniil Gannota <daniil.gannota.contact@gmail.com>
2016-08-28 19:36:16 +0300

    0.5.1
     - added ‘content’ optimisations

---
+++ content/script.js
@@ -0,0 +1,21 @@
+$('document').ready(function(){
+	
+	/*----------------------------------- Loading -----------------------------------*/
+    $(document).ajaxStart(function(){$("#wait").css("display", "block");	$( ".inside" ).html("");});
+    $(document).ajaxComplete(function(){$("#wait").css("display", "none");});
+	
+	
+	/*----------------------------------- Content (loaded externally) -----------------------------------*/
+	$.ajaxSetup ({cache: false});
+	
+	$('.menEdu').click(function() {$( ".inside" ).load( "../content/content.html #education");	$('.featured').toggle();});
+	$('.menTechSkills').click(function() {$( ".inside" ).load( "../content/content.html #techskills");	$('.featured').toggle();});	
+	$('.menWorkExp').click(function() {$( ".inside" ).load( "../content/content.html #workexp");	$('.featured').toggle();});
+	$('.menProj').click(function() {$( ".inside" ).load( "../content/content.html #proj");	$('.featured').toggle();});
+	$('.menCovLet').click(function() {$( ".inside" ).load( "../content/content.html #covlet");	$('.featured').toggle();});
+	$('.menPerStat').click(function() {$( ".inside" ).load( "../content/content.html #perstat");	$('.featured').toggle();});
+	
+	
+	/*----------------------------------- Simple Transition: when to be closed -----------------------------------*/
+	$('.transition').click(function() {$('.featured').toggle();});
+});		
